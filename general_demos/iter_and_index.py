ac1a6b9aeaa07132faf50d2c62d71bf19554ecbe
itay zohar <dracula753@gmail.com>
2021-12-18 16:50:48 +0200

    iter and getitem example

--- general_demos/iter_and_index.py
+++ general_demos/iter_and_index.py
@@ -34,8 +34,57 @@ def simple_generator():
     i = 0
     while i < 100:
         a = yield i
-        if a: i = a
-        else: i += 1
+        if a:
+            i = a
+        else:
+            i += 1
 
 
 sg = simple_generator()
+
+
+# example - DrugManager
+class DrugManager:
+    def __init__(self, schedual: dict) -> None:
+        self.schedual = schedual
+        self.days = (
+            "sunday",
+            "monday",
+            "tuesday",
+            "wednesday",
+            "thursday",
+            "friday",
+            "saturday",
+        )
+
+    def add_drug(self, day, drug_name):
+        self.schedual[day] = drug_name
+
+    def __getitem__(self, day):
+        return self.schedual[day]
+
+    def __setitem__(self, day, drug):
+        self.schedual[day] = drug
+
+    def __iter__(self):
+        self.day = -1
+        return self
+
+    def __next__(self):
+        self.day += 1
+        if self.day >= len(self.days):
+            raise StopIteration
+        return self.schedual.get(self.days[self.day], "no drug today")
+
+
+d = DrugManager({"sunday": "a", "tuesday": "b", "friday": "c"})
+
+d = iter(d)
+for n in d:
+    print(n)
+
+
+d["monday"] = "z"
+
+for n in d:
+    print(n)
