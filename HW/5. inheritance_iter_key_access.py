f09302552ab2fcf87f72a541e36885bbab89d8fb
itay zohar <dracula753@gmail.com>
2021-12-20 15:45:22 +0200

    add iter hw

--- HW/5. inheritance_iter_key_access.py
+++ HW/5. inheritance_iter_key_access.py
@@ -12,3 +12,17 @@ class Student(Person):
 
     def add_grade(prof, grade):
         self.grades[prof] = grade
+
+    def __getitem__(self, item):
+        return self.grades[item]
+
+    def __setitem__(self, item, value):
+        self.grades[item] = value
+
+    def __iter__(self):
+        return ((prof, grade) for prof, grade in self.grades.items())
+
+
+s = Student("miri", 0, 33, {"math": 90, "python": 80}, "TAU")
+for prof, grade in s:
+    print(f"{prof}: {grade}")
